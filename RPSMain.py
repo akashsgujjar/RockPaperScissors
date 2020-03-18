a40fd5e2ecde88a2f8f5e356c3c23be45a4a5927
akashgujjar <akashsgujjar@gmail.com>
2020-03-18 14:28:32 -0700

    basic func

--- RPSMain.py
+++ RPSMain.py
@@ -1,1 +1,23 @@
-print("hi")
+from random import seed, randint
+from random import random
+
+
+def main():
+    print("Akash")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def compMove():
+    value = randint(1, 3)
+    if value == 1:
+        print "Computer says Rock"
+    if value == 2:
+        print "Computer says Paper"
+    if value == 3:
+        print "Computer says Scissors"
+
+
+compMove()
